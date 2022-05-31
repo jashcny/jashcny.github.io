77a7be8d9f1dead4fb67eb298e5eea1bbccf4f2a
jashcny <hxh@miami.edu>
2022-05-31 11:38:43 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,34 +1,37 @@
+
+
 $(document).ready(function(){
 
+
 $("div.info").hide();
-$(".button-1").css("background-color","rgb(229,0,40)");
-$(".button-2").css("background-color","#989898");
-$(".button-3").css("background-color","#989898");
+$(".button-1").css("color","black");
+$(".button-2").css("color","#999999");
+$(".button-3").css("color","#999999");
 $("div.video").hide();
 
 $(".button-1").click(function(){
   $("div.interactive").show("slow");
-	$(".button-1").css("background-color","rgb(229,0,40)");
-	$(".button-2").css("background-color","#989898");
-	$(".button-3").css("background-color","#989898");
+	$(".button-1").css("color","black");
+	$(".button-2").css("color","#999999");
+	$(".button-3").css("color","#999999");
   $("div.info").hide("slow");
 	$("div.video").hide("slow");
 });
 
 $(".button-2").click(function(){
   $("div.interactive").hide("slow");
-	$(".button-1").css("background-color","#989898");
-	$(".button-2").css("background-color","rgb(24,144,237)");
-	$(".button-3").css("background-color","#989898");
+	$(".button-1").css("color","#999999");
+	$(".button-2").css("color","black");
+	$(".button-3").css("color","#999999");
   $("div.info").show("slow");
 	$("div.video").hide("slow");
 });
 
 $(".button-3").click(function(){
   $("div.interactive").hide("slow");
-	$(".button-1").css("background-color","#989898");
-	$(".button-2").css("background-color","#989898");
-	$(".button-3").css("background-color","rgb(254,197,54)");
+	$(".button-1").css("color","#999999");
+	$(".button-2").css("color","#999999");
+	$(".button-3").css("color","black");
   $("div.info").hide("slow");
 	$("div.video").show("slow");
 });
